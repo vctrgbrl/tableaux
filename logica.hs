d03f87fbe57a28b24df938f02fb858b3e0c834ba
Victor Sant'Ana <victorgabrielss20@gmail.com>
2024-05-08 12:26:07 -0300

    definições de expressão recursivas

--- logica.hs
+++ logica.hs
@@ -1,24 +1,31 @@
--- {(a), (a, implica, b), (b)}
--- {(a,true), (a, implica, b), (b, false)}
--- {(a, true), (a, implica, b), (b, false), (a, false)} -> x
--- {(a, true), (a, implica, b), (b,false), (b, true)} -> x
-
 import Data.List
 
-fact :: Integer -> Integer
-fact 0 = 1
-fact x = x * fact (x - 1)
+data Expressao = Variavel String | Implicacao Expressao Expressao | ELogico Expressao Expressao | OuLogico Expressao Expressao | Negacao Expressao
 
-data Expressao = Variavel String | Implicacao String String
-
+-- (p∨(q∧r))→((p∨q)∧(p∨r))
+-- [(Implicacao (OuLogico (Variavel "p") (ELogico (Variavel "q") (Variavel "r"))) ( ELogico (OuLogico (Variavel "p" "q")) (OuLogico (Variavel "p" "r")) ), False)]
 argument :: [(Expressao, Bool)]
 -- argument = [(Variavel "a", True), (Implicacao (Variavel "a") (Variavel "b"), True), (Variavel "b", False)]
-argument = [(Variavel "a", True), (Implicacao "a" "b", True), (Variavel "b", False)]
+-- argument = [(Variavel "a", True), (Implicacao (Variavel "a") (Variavel "b"), True), (Variavel "b", False)]
+
+argument = [(Implicacao (OuLogico (Variavel "p") (ELogico (Variavel "q") (Variavel "r"))) (ELogico (OuLogico (Variavel "p") (Variavel "q")) (OuLogico (Variavel "p") (Variavel "r"))), False)]
+
+-- argument = [(Implicacao (Variavel "b") (ELogico (Variavel "a") (OuLogico (Variavel "b") (Variavel "a"))), False)]
 
 evaluate :: [(Expressao, Bool)] -> [(Expressao, Bool)] -> Bool
-evaluate ((Variavel x, v) : xs) vars = evaluate xs (vars ++ [(Variavel x, v)])
-evaluate ((Implicacao x y, True) : xs) vars = evaluate xs (vars ++ [(Variavel x, False)]) && evaluate xs (vars ++ [(Variavel y, True)])
-evaluate ((Implicacao x y, False) : xs) vars = evaluate xs (vars ++ [(Variavel x, True), (Variavel y, False)])
+evaluate ((Variavel x, v) : exprs) vars = evaluate exprs (vars ++ [(Variavel x, v)])
+--
+evaluate ((Implicacao expX expY, True) : exprs) vars = evaluate (exprs ++ [(expX, False)]) vars && evaluate (exprs ++ [(expY, True)]) vars
+evaluate ((Implicacao expX expY, False) : exprs) vars = evaluate (exprs ++ [(expX, True), (expY, False)]) vars
+--
+evaluate ((ELogico expX expY, True) : exprs) vars = evaluate (exprs ++ [(expX, True), (expY, True)]) vars
+evaluate ((ELogico expX expY, False) : exprs) vars = evaluate (exprs ++ [(expX, False)]) vars && evaluate (exprs ++ [(expY, False)]) vars
+--
+evaluate ((OuLogico expX expY, True) : exprs) vars = evaluate (exprs ++ [(expX, True)]) vars && evaluate (exprs ++ [(expY, True)]) vars
+evaluate ((OuLogico expX expY, False) : exprs) vars = evaluate (exprs ++ [(expX, False), (expY, False)]) vars
+--
+evaluate ((Negacao expX, v) : exprs) vars = evaluate (exprs ++ [(expX, not v)]) vars
+--
 evaluate [] vars = hasContradiction $ sortVars vars
 
 sortVars = sortBy (\(Variavel a, v) (Variavel b, g) -> compare a b)
